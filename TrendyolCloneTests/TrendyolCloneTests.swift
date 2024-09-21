0a25ddb7a06eb0c03e8e2ab7c1fc262384f9b168
Mete <metehangurgentepe@gmail.com>
2024-09-21 13:09:22 +0300

    Initial Commit

---
+++ TrendyolCloneTests/TrendyolCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrendyolCloneTests.swift
+//  TrendyolCloneTests
+//
+//  Created by Metehan Gürgentepe on 21.09.2024.
+//
+
+import Testing
+@testable import TrendyolClone
+
+struct TrendyolCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
